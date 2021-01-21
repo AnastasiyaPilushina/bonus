7e2f02fedb60605c095c93b2222564810b1cd290
AnastasiyaPilushina <pilushina22@gmail.com>
2021-01-21 15:01:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int balance = 500;
+       int refill = 2000;
+       int bonus = 100;
+       int total = balance + refill / bonus;
+        System.out.println(total);
+
+    }
+}
