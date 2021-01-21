a42e8871534f96cbf10996ce7f4d2bf963d5c6d0
AnastasiyaPilushina <pilushina22@gmail.com>
2021-01-21 17:20:49 +0300

    Added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-       int balance = 500;
-       int refill = 2000;
-       int bonus = 100;
-       int total = balance + refill / bonus;
-        System.out.println(total);
+        boolean introduced = true;
+        int bonus;
+        if (introduced) {
+            bonus = 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = 500;
+        int refill = 2000;
+        int total =( balance + refill) / bonus;
+        int limit = 1000;
+        if (refill > limit) {
+            refill = limit;
+        }
 
+        System.out.println(total);
     }
+
+
 }
